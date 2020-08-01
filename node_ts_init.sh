86dad8b245863eea86b2a4f9dc00d25aeb0338d0
Adam Michalak <adam.michalak.dev@gmail.com>
2020-08-01 13:02:06 +0200

    initialize git repo

--- node_ts_init.sh
+++ node_ts_init.sh
@@ -23,3 +23,31 @@ sed 's#"test":.*#"start": "npm run build:live",\n    "build": "tsc -p .",\n    "
 mv package.tmp package.json
 
 printf '\e[1;32m%-6s\e[m' "Live compile + run set successfully."
+printf "\n\n"
+
+# Initialize empty git repository
+git init
+
+# Ignore files related to node and vsc
+curl https://www.toptal.com/developers/gitignore/api/node,visualstudiocode > .gitignore
+echo "lib/" >> .gitignore
+
+# Enable user to add remote repository
+read -p "Do you want to track remote repository?[Y/n] " repoAnswer
+if [ "$repoAnswer" = "Y" ] || [ "$repoAnswer" = "y" ] || [ "$repoAnswer" = "" ]; then
+    read -p "Feed me with link to the repository: (press q to quit) " repoLink
+    #enable user to quit here
+    if [ "$repoLink" = "q" ]; then
+        echo "Quitting connection to remote repository"
+    else
+        git remote add origin $repoLink
+    fi
+fi
+
+# Initial commit + switch to development branch
+git add .
+git commit -m "initial commit with project template generated"
+git checkout -b development
+
+printf "\n"
+printf '\e[1;32m%-6s\e[m' "Git repo initialized."
